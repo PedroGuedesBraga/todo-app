fd7ffb6f4c5b13c941761dda0bb4240a31ad6f1f
unknown <pedro.braga@ccc.ufcg.edu.br>
2018-09-21 22:16:14 -0300

    First commit + Login Component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container-fluid">
+    <nav class="navbar navbar-light bg-light">
+      <a href="#" class="navbar-brand">Turbo To-Do</a>
+    </nav>
+    <Login></Login>
+
+    
+    
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/login.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Login
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
