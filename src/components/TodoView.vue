3b61f46953c72b2d5c871d13a189d4bb0be91a5a
unknown <pedro.braga@ccc.ufcg.edu.br>
2018-10-04 00:37:56 -0300

    Consegue fazer requests para o server/ fix cors thing

--- src/components/TodoView.vue
+++ src/components/TodoView.vue
@@ -4,6 +4,9 @@
         
         <div class="col-12 text-center">
             <br>
+            <h6 class="text-right">Você está logado como: {{userName}}</h6>
+            <h6 class="text-right font-weight-light">Id: {{userId}}</h6>
+            <br>
             <button type="button" class="btn btn-warning btn-md btn-block">Adicionar To-Do</button>
         </div>
         <div class="col-6 text-center">
@@ -34,11 +37,28 @@
 <script>
 
 import Todo from './Todo.vue'
+import axios from 'axios'
 
 export default {
     name: 'TodoView',
     components: {
         Todo
+    },
+    data(){
+        return {
+            userName: 'Nome do usuario',
+            userId: 'Id do usuario'
+        }
+    },
+
+    
+    created(){
+        axios.get('http://localhost:5000/panel/userInfo', {withCredentials: true})
+        .then((response) => {
+            this.userName = response.data.username;
+            this.userId = response.data._id;
+            console.log('nome do usuario: ' + response.data)
+        })
     }
 }
 </script>
